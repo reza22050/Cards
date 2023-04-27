c689c36dccd872b27c20350aa0d70be8ac048923
Reza <RezaTaghipour99@gmail.com>
2023-04-27 18:36:53 +0430

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts = [{
+    title: 'tree', 
+    imageUrl: 'assets/images/tree.jpeg', 
+    username: 'nature', 
+    content: 'I saw the tree'
+  }, 
+  {
+    title: 'mountain', 
+    imageUrl: 'assets/images/mountain.jpeg', 
+    username: 'mountain', 
+    content: 'I saw the mountain'
+  }, 
+  {
+    title: 'biking', 
+    imageUrl: 'assets/images/biking.jpeg', 
+    username: 'biking', 
+    content: 'I saw the bicycle'
+  }]
+
+
+  getPosts(){
+    return this.posts;
+  }
 }
